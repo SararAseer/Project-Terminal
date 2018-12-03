0af2e7393d7698fff4e9771eb3a0a568706017f4
ychan3 <ychan3@stuy.edu>
2018-12-03 11:44:37 -0500

    this is hard

--- funcs.h
+++ funcs.h
@@ -1,17 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char** pl(char *line, char* delim);
+char** pl(char *line, char* delim); //Parses the line base on the delimiter given so that it retuns an char**
 
 void pip(char *first, char *second);
 
-int exec(char * entry);
+int exec(char * entry); //runs a command given a char*
 
-int new_out(char* entry, char* file);
+int new_out(char* entry, char* file); // redirects a command into a file by changing the stdout
 
-int new_in(char* entry, char* file);
+int new_in(char* entry, char* file); // redirects a file into the stdin
 
-char * remove_n(char * entry);
+char * remove_n(char * entry); // removes a \n from the end of a string
 
 void check(char **line);
 
